6a4f8e6769ace8a73eb46a997d9af9111d7811ef
Marcwos <ubituna22@gmail.com>
2025-10-21 14:25:33 -0500

    Creacion de graphql

---
+++ graphql/src/usuarios/entities/usuario.entity.ts
@@ -0,0 +1,8 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Usuario {
+  @Field(() => Int, { description: 'Example field (placeholder)' })
+  exampleField: number;
+  // HOLA
+}
